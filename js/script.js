91d07df32706cf38735bf94e49c0c150ec593b07
brightestsirius <katherina.shypovskaya@gmail.com>
2021-04-09 17:11:53 +0300

    fix closed btn tag

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const antmanData = {
 	name: "Ant-Man",
-	info: "Ant-Man is a 2015 American superhero film based on the Marvel Comics characters of the same name: Scott Lang and Hank Pym."
+	info: "Ant-Man is a 2015 American superhero film based on the Marvel Comics characters of the same name: Scott Lang and Hank Pym.",
+	img: "antman"
 }
